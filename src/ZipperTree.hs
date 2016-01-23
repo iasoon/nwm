d97591cfa7da170c8006374e162d90a75996063d
Ilion Beyst <ilion.beyst@gmail.com>
2016-01-23 01:17:02 +0100

    Introducing the zipper monad

--- src/ZipperTree.hs
+++ src/ZipperTree.hs
@@ -14,37 +14,48 @@ data Sign = Neg | Pos deriving (Show)
 
 data Direction = Dir Axis Sign deriving (Show)
 
-data Context a = Context [Context a] Direction Rational (ZipperTree a)
-    deriving (Show)
+data Ctx a = Ctx Direction Rational (ZipperTree a) deriving (Show)
 
-data ZipperTree a = Zipper [Context a] (Node a)
-    deriving (Show)
+type Context a = Zipper a (Ctx a)
 
-data Node a = Empty | Leaf a | Fork Axis Rational (ZipperTree a) (ZipperTree a)
-            deriving (Show)
+data Zipper c a = Zipper [Context c] a deriving (Show)
 
-appendCtx :: [Context a] -> ZipperTree a -> ZipperTree a
+instance Functor (Zipper c) where
+    fmap f (Zipper ctx a) = Zipper ctx (f a)
+
+instance Applicative (Zipper c) where
+    pure a = Zipper [] a
+    (<*>) (Zipper ctx f) = appendCtx ctx . fmap f
+
+instance Monad (Zipper c) where
+    (Zipper ctx a) >>= f = appendCtx ctx $ f a
+
+appendCtx :: [Context c] -> Zipper c a -> Zipper c a
 appendCtx cs (Zipper ctx t) = Zipper (ctx ++ cs) t
 
+type ZipperTree a = Zipper a (Node a)
+
+data Node a = Empty | Leaf a | Fork Axis Rational (ZipperTree a) (ZipperTree a)
+    deriving (Show)
+
+applyContext :: ZipperTree a -> Context a -> ZipperTree a
+applyContext t ctx = ctx >>= \(Ctx d p t') -> insert d p t' t
+
 ctxToTree :: [Context a] -> Maybe (ZipperTree a)
-ctxToTree []                       = Nothing
-ctxToTree ((Context ctx _ _ t):cs) = Just $ foldl' (flip applyCtx) bottom cs
-    where bottom = appendCtx ctx t
+ctxToTree []     = Nothing
+ctxToTree (c:cs) = Just $ foldl' applyContext (c >>= \(Ctx _ _ t) -> t) cs
 
 asSibling :: Direction -> Rational -> [Context a] -> [Context a]
-asSibling d p ctx = maybeToList $ Context [] d p <$> ctxToTree ctx
+asSibling d p ctx = maybeToList $ pure . Ctx d p <$> ctxToTree ctx
 
 fork :: Axis -> Rational -> ZipperTree a -> ZipperTree a -> ZipperTree a
 fork a p (Zipper ctx Empty) t = appendCtx (asSibling (Dir a Neg) p ctx) t
 fork a p t (Zipper ctx Empty) = appendCtx (asSibling (Dir a Pos) (1-p) ctx) t
-fork a p l r                  = Zipper [] $ Fork a p l r
+fork a p l r                  = pure $ Fork a p l r
 
 insert :: Direction -> Rational -> ZipperTree a -> ZipperTree a -> ZipperTree a
 insert (Dir a Neg) p l r = fork a p l r
 insert (Dir a Pos) p r l = fork a (1-p) l r
-
-applyCtx :: Context a -> ZipperTree a -> ZipperTree a
-applyCtx (Context ctx d p t') t = appendCtx ctx $ insert d p t' t
 
 delete :: Eq a => a -> ZipperTree a -> ZipperTree a
 delete _ (Zipper ctx Empty) = Zipper ctx $ Empty
@@ -61,4 +72,4 @@ node :: ZipperTree a -> Node a
 node (Zipper _ n) = n
 
 leaf :: a -> ZipperTree a
-leaf a = Zipper [] (Leaf a)
+leaf = pure . Leaf
